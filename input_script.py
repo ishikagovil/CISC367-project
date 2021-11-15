2d0de9734f6c16ad5bb401c647d6b3d74c6595e2
robertsammataro <robbysam@udel.edu>
2021-11-15 10:23:33 -0500

    Added encoding to input_script.py
    Would not run on Windows systems without explicitly defining utf-8 encoding on input_script.py line 3

--- input_script.py
+++ input_script.py
@@ -1,4 +1,4 @@
 import csv
 def read_input_csv(directory_name, filename):
-    csvreader = csv.reader(open(directory_name + "/" + filename, 'rt'))
+    csvreader = csv.reader(open(directory_name + "/" + filename, 'rt', encoding="utf-8"))
     return csvreader
