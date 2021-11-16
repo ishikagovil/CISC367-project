edb7671ecbb44585051b8192a1b1a6874e44ee53
matt041399 <mattnovo@udel.edu>
2021-11-16 09:44:53 -0500

    slightly faster

--- authors_script.py
+++ authors_script.py
@@ -7,11 +7,12 @@ def get_user_info(csvreader):
    csvreader.__next__()
    for line in csvreader:
         print(line[0])
-        angryMessage = te.get_emotion(line[3]).get("Angry")
-        happyMessage = te.get_emotion(line[3]).get("Happy")
-        sadMessage = te.get_emotion(line[3]).get("Sad")
-        surprisedMessage = te.get_emotion(line[3]).get("Surprise")
-        fearMessage = te.get_emotion(line[3]).get("Fear")
+        message =  te.get_emotion(line[3])
+        angryMessage = message.get("Angry")
+        happyMessage = message.get("Happy")
+        sadMessage = message.get("Sad")
+        surprisedMessage = message.get("Surprise")
+        fearMessage = message.get("Fear")
         if line[2] in user_dict.keys():
            totalAngry = user_dict[line[2]].avg_angry_score * user_dict[line[2]].total_messages
            totalHappy = user_dict[line[2]].avg_happy_score * user_dict[line[2]].total_messages
@@ -19,34 +20,12 @@ def get_user_info(csvreader):
            totalSurprised = user_dict[line[2]].avg_surprised_score * user_dict[line[2]].total_messages
            totalFear = user_dict[line[2]].avg_fear_score * user_dict[line[2]].total_messages
            
-           if(totalAngry + angryMessage == 0):
-               avgAngry = 0
-           else:
-               avgAngry = (totalAngry + angryMessage) / (user_dict[line[2]].total_messages + 1)
-
-
-           if(totalHappy + happyMessage == 0):
-               avgHappy = 0
-           else:
-               avgHappy = (totalHappy + happyMessage) / (user_dict[line[2]].total_messages + 1)
            
-
-           if(totalSad + sadMessage == 0):
-               avgSad = 0
-           else:
-               avgSad = (totalSad + sadMessage) / (user_dict[line[2]].total_messages + 1)
-           
-
-           if(totalSurprised + surprisedMessage == 0):
-               avgSurprised = 0
-           else:
-               avgSurprised = (totalSurprised + surprisedMessage) / (user_dict[line[2]].total_messages + 1)
-            
-           
-           if(totalFear + fearMessage == 0):
-               avgFear = 0
-           else:
-                avgFear = (totalFear + fearMessage) / (user_dict[line[2]].total_messages + 1)
+           avgAngry = (totalAngry + angryMessage) / (user_dict[line[2]].total_messages + 1)
+           avgHappy = (totalHappy + happyMessage) / (user_dict[line[2]].total_messages + 1)
+           avgSad = (totalSad + sadMessage) / (user_dict[line[2]].total_messages + 1)
+           avgSurprised = (totalSurprised + surprisedMessage) / (user_dict[line[2]].total_messages + 1)
+           avgFear = (totalFear + fearMessage) / (user_dict[line[2]].total_messages + 1)
            
 
            user_dict[line[2]].avg_angry_score = avgAngry
